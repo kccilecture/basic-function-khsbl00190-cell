d943ef76f7b4436bed4edc30d839b25d411a8580
khsbl <khsbl00190@gmail.com>
2026-03-18 12:04:08 +0900

    숙제완료

--- basic_func/basic_func.py
+++ basic_func/basic_func.py
@@ -1,26 +1,38 @@
+import math
+
 def add(a, b):
-    pass
+    return (a+b)
 
 
 def sub(a, b):
-    pass
+    return (a-b)
 
 
 def mul(a, b):
-    pass
+    return (a*b)
 
 
 def div(a, b):
-    pass
+    return(a/b)
 
 
 def power(base, pow):
-    pass
+    return base ** pow
 
 
 def square(base):
-    pass
+    return base ** 2
+
+result = square(100)
+print(result)  # 결과: [10.0, -10.0]
 
 
 def greet(이름="낯선자", 나이=20):
-    pass
+    if 나이 < 5:
+        return f"안녕 {이름}!"
+    
+    elif 나이 >= 50:
+        return f"안녕하십니까 {이름}!"
+    
+    else:
+        return f"안녕하신가 {이름}!"
